3b4540f688bb1f0e50588edecba6840f98c00b3c
Mateus Costa <msc.mateus@hotmail.com>
2022-08-11 17:07:06 -0500

    o usuário deverá realizar novo calculo #4

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,28 @@
+operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for nultiplication
+/ for division
+''')
+
 number_1 = int(input('Enter yout first number:'))
 number_2 = int(input('Enter yout second number:'))
 
-print('{} + {} = '.format(number_1, number_2))
-print(number_1 + number_2)
+if operation == '+':
+   print('{} + {} = '.format(number_1, number_2))
+   print(number_1 + number_2)
 #Subtraction
-print('{}-{} = '.format(number_1, number_2))
-print(number_1 - number_2)
+elif operation == '-':
+   print('{}-{} = '.format(number_1, number_2))
+   print(number_1 - number_2)
 #Multiplication
-print('{}*{} = '.format(number_1, number_2))
-print(number_1 * number_2)
+elif operation == '*':
+   print('{}*{} = '.format(number_1, number_2))
+   print(number_1 * number_2)
 #Division
-print('{}/{} = '.format(number_1, number_2))
-print(number_1 + number_2)
+elif operation == '/':
+   print('{}/{} = '.format(number_1, number_2))
+   print(number_1 + number_2)
+else:
+   print('You have not typed a valid operator, please run the program again.')
